e9797943c3e40128e1916c326606f8d12fa75d00
cissalcliu@gmail.com <cissalcliu@gmail.com>
2017-11-15 13:54:34 +0800

    修复bug

--- fcat-angular/src/main.ts
+++ fcat-angular/src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import assign from 'object-assign';
+assign({}, {});
 
 if (environment.production) {
   enableProdMode();
